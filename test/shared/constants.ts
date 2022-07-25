6fcb0a6791df1c86a2fa725f86734206f22f8f4f
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-07-25 18:06:00 +0500

    chore: add liquidation percantage const

--- test/shared/constants.ts
+++ test/shared/constants.ts
@@ -16,3 +16,5 @@ export const NINE_MONTHTS_DOWN_PAYMENT_PERCENTAGE: number = 12; // 12%
 export const THREE_MONTHTS_MONTHLY_PERCENTAGE: number = 33; // 33%
 export const SIX_MONTHTS_MONTHLY_PERCENTAGE: number = 165; //16.5%
 export const NINE_MONTHTS_MONTHLY_PERCENTAGE: number = 11; // 11%
+
+export const LIQUIDATION_PERCENTAGE: number = 95; // 95%
