b061a81ce94457d00ff881a175de11f47f635fde
ahmedali <imahmedalibhatti@gmail.com>
2022-08-04 13:54:54 +0500

    test(utils): ignore in coverage

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,8 @@
 module.exports = {
   istanbulReporter: ["html", "lcov"],
   skipFiles: ["test"],
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
 };
